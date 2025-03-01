40ea3e230cbaafb6cb5b932394d2374df677b81a
Prathamesh Devhare <prathameshd@quartom.io>
2025-03-01 23:09:04 +0530

    Implemented circle area calculation

--- geometry_calculator.py
+++ geometry_calculator.py
@@ -12,4 +12,6 @@ if __name__ == "__main__":
 
     calculator = GeometryCalculator()
 
-    
+    length = 10
+    width = 6
+    print(f"The area of the rectangle with length {length} and width {width} = {calculator.calculate_rectangle_area(length, width)}")
