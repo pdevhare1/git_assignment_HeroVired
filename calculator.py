8976842ba3f688b3f822a3dbf5b4f1482ce4b8e5
Prathamesh Devhare <prathameshd@quartom.io>
2025-03-01 22:34:31 +0530

    🐞 Fix divide by zero bug

--- calculator.py
+++ calculator.py
@@ -11,7 +11,10 @@ class Calculator:
         return a * b
 
     def divide(self, a, b):
-        return a / b
+      if b == 0:
+          raise ValueError("Cannot divide by zero.")
+      return a / b
+
 
 if __name__ == "__main__":
     calculator = Calculator()
